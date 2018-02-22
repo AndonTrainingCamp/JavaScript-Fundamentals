6f7b6cc7f91768bad31295e3abfc4770893eaf10
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-02-22 23:57:08 +0200

    Add files via upload

---
+++ Arrays/SelectionSort.js
@@ -0,0 +1,28 @@
+/*jslint devel: true */
+/*jslint es6 */
+'use strict';
+
+let inputArr = [10, 1, 0, -4, 8, -3, 5];
+solve(inputArr);
+
+function solve(args) {
+    let minNum = args[0],
+        lastIndex, check = 0,
+        j,i;
+    for (j = 0; j < args.length - 1; j++) {
+        for (i = j; i < args.length; i++) {
+            if (args[i] < minNum) {
+                minNum = args[i];
+                lastIndex = i;
+                check++;
+            }
+        }
+        if (check != 0) {
+            args[lastIndex] = args[j];
+        }
+        args[j] = minNum;
+        minNum = args[j+1];
+        check = 0;
+        console.log(args[j]);
+    }
+}
