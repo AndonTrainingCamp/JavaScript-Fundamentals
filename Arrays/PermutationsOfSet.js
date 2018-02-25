f67c28b1b0485042c1d7b4aaba5ed176d392bff9
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-02-25 15:32:59 +0200

    Add files via upload

---
+++ Arrays/PermutationsOfSet.js
@@ -0,0 +1,36 @@
+/*jslint devel: true */
+/*jslint es6 */
+'use strict';
+
+let inputNum = 5;
+solve(inputNum);
+
+function solve(num) {
+    let arrNums = [],
+        line = [],
+        numFact = calcFactorial(num),
+        i, j, k, m;
+    for (i = 0; i < num; i++) {             //Initialize an array with n sorted numbers with copy in line
+        arrNums[i] = i + 1;
+        line[i] = arrNums[i];
+    }
+    for (j = 0; j < numFact / (num - 1); j++) {
+        for (k = 0; k < num - 1; k++) {
+            line[k] = arrNums[k + 1];
+            line[k + 1] = arrNums[k];
+            console.log('{', line.join(', '), '}');
+                for (m = 0; m < num; m++) {
+                    arrNums[m] = line[m];
+                }
+        }
+    }
+}
+
+function calcFactorial (n) {
+    let result = n,
+        i;
+    for (i = 1; i < n; i++) {
+        result = result * (n - i); 
+    }
+    return result;
+}
