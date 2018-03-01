b58a2ccb8ccdc7164bb8587b19dba67083f5805c
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-03-01 19:59:25 +0200

    Add files via upload

--- Functions/EnglishDigit.js
+++ Functions/EnglishDigit.js
@@ -2,27 +2,14 @@
 /*jslint es6 */
 'use strict';
 
-solve(123456789);
+solve(1234567);
 
 function solve(inputNum) {
     lastDigit(inputNum); 
 }
 
 function lastDigit(num) {
-    let digit,
-        numString = num.toString();
-    switch (+(numString[numString.length - 1])) {
-        case 0: digit = 'zero'; break;
-        case 1: digit = 'one'; break;
-        case 2: digit = 'two'; break;
-        case 3: digit = 'three'; break;
-        case 4: digit = 'four'; break;
-        case 5: digit = 'five'; break;
-        case 6: digit = 'six'; break;
-        case 7: digit = 'seven'; break;
-        case 8: digit = 'eight'; break;
-        case 9: digit = 'nine'; break;
-    }
-    console.log(digit);
-    return;
+    let digit = ['zero','one','two','three','four','five','six','seven','eight','nine'],
+        index = num % 10;
+        console.log(digit[index]);
 }
