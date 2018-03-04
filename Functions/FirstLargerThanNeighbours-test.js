697ea02628499e86d744ca50d5b070c80f353bbf
Nikola Vushkov <bateshot@gmail.com>
2018-03-04 22:49:06 +0200

    TDD :)

---
+++ Functions/FirstLargerThanNeighbours-test.js
@@ -0,0 +1,13 @@
+function solve (arr) {
+    //TODO
+    return false;
+}
+
+console.log(solve([]) === -1);
+console.log(solve([1]) === 0);
+console.log(solve([1,1]) === -1);
+console.log(solve([1,2,3]) === 2);
+console.log(solve([4,3,2]) === 0);
+console.log(solve([1, 2, 3, 2, 1]) === 2);
+console.log(solve([-1, -2, -1]) === 0);
+console.log(solve([-2, -1, 3, -2]) === 2);
