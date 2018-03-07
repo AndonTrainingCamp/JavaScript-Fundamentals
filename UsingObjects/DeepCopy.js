3fac9dc35a2ef9b7fbe332bf7e8b5985adf72284
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-03-07 18:03:12 +0200

    Add files via upload

--- UsingObjects/DeepCopy.js
+++ UsingObjects/DeepCopy.js
@@ -1,21 +1,26 @@
+/*jslint devel: true */
+/*jslint es6 */
 'use strict';
+
 let copyObj = {};
 let o = {
-	a: 1, b: {c: {x: 0}, d: {e: 3, f: 4}}
+    a: [1,2,3], b: {i: 4, c: {d: 0, k: {x: 0, e: {r: [{q:1},{w:2},{z:3}]}}}}
 };
 recrsvCopy(o,copyObj);
-o.b.c.x = 'original';
-copyObj.b.c.x = 'copy';
-console.log(copyObj);
+o.a[1] = 22;
+copyObj.b.c.k.e.r[2].z = 888;
 console.log(o);
+console.log(copyObj);
+console.log(copyObj.b.c.k.e.r[2].z);
 
 function recrsvCopy(obj,copyObj) {
-	for (let el in obj) {
-		if (typeof obj[el] === typeof obj) {
+    for (let el in obj) {
+		if (typeof obj[el] === typeof obj && !(Array.isArray(obj[el]))) {
 			copyObj[el] = {};
-			for (let el2 in obj[el]) {
-				recrsvCopy(obj[el],copyObj[el]);
-			}
+			recrsvCopy(obj[el],copyObj[el]);
+		} else if (Array.isArray(obj[el])) {
+			copyObj[el] = [];
+			recrsvCopy(obj[el],copyObj[el]);
 		} else if (typeof obj[el] !== typeof obj) {
 			copyObj[el] = obj[el];
 		}
