35ea8487aa3071738844d589eabfef5c52a05689
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-03-09 00:19:10 +0200

    Update GroupPeople.js

--- UsingObjects/GroupPeople.js
+++ UsingObjects/GroupPeople.js
@@ -15,14 +15,14 @@ console.log(groupByAge(peopleInput));
 
 function groupByAge(people) {
     let grouped = {},
-        ageArr = [],    //Takes only ages
+        ageArr = [],
         i;
-    for (i = 0; i < people.length; i++) {
+    for (i = 0; i < people.length; i++) {   //Copy ages from people.age to ageArr
         ageArr[i] = people[i].age;
     }
     let nextAge = ageArr[0],
         j;
-    for (j = 0; j < ageArr.length - 1; j++) {
+    for (j = 0; j < ageArr.length - 1; j++) {      //Sort ages in ageArr
         for (i = j + 1; i < ageArr.length; i++) {
             if (nextAge > ageArr[i]) {
                 ageArr[j] = ageArr[i];
@@ -35,7 +35,7 @@ function groupByAge(people) {
     let sameAge = 0,
         hasArr = false,
         k;
-    for (i = 0; i < ageArr.length;) {
+    for (i = 0; i < ageArr.length;) {                //Group people by order from sorted ageArr
         for (j = 0, k = 0; j < people.length; j++) {
             if (ageArr[i] === people[j].age) {
                 if (!hasArr) {
