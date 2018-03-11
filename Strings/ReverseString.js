e1d9305fb634a799198e7406ea1899a744751fdb
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-03-11 21:42:01 +0200

    Add files via upload

--- Strings/ReverseString.js
+++ Strings/ReverseString.js
@@ -1,1 +1,16 @@
+/*jslint devel: true */
+/*jslint es6 */
+'use strict';
 
+const input = ['string'];
+
+function reverseString(stringArr) {
+    let stringInput = stringArr[0],
+        reversed = '',
+        i, j;
+    for (i = 0, j = stringInput.length - 1; i < stringInput.length; i++, j--) {
+        reversed += stringInput[j];
+    }
+    return reversed;
+}
+console.log(reverseString(input));
