e73c02f6ac19112d3abbdcdf643f85ddc93b5eea
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-03-12 15:27:47 +0200

    Add files via upload

---
+++ Strings/CorrectBrackets.js
@@ -0,0 +1,23 @@
+/*jslint devel: true */
+/*jslint es6 */
+'use strict';
+
+const input = ['(()))('];
+
+function checkBrackets(stringArr) {
+    let expression = stringArr[0];
+    let openBra = 0,
+        i;
+    for (i = 0; i < expression.length; i++) {
+        if (expression[i] === '(') {
+            openBra++;
+        } else if (expression[i] === ')') {
+            openBra--;
+            if (openBra < 0) {
+                return 'Incorrect';
+            }
+        }
+    }
+    return openBra === 0 ? 'Correct' : 'Incorrect';
+}
+console.log(checkBrackets(input));
