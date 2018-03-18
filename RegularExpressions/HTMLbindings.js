9d286e576aac881b269f6951b716a95dc2c25f86
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-03-18 13:26:27 +0200

    Add files via upload

--- RegularExpressions/HTMLbindings.js
+++ RegularExpressions/HTMLbindings.js
@@ -10,16 +10,19 @@ let input = [
 // <a data-bind-content="name" data-bind-href="link" data-bind-class="name" href="http://telerikacademy.com" class="Elena">Elena</а>
 
 String.prototype.htmlBind = function (dataObj) {
-    let result,
+    let result = this,
         dataBind = /data-bind-(\w+)="(\w+)"/g,
         matchesArr;
-    while ((matchesArr = dataBind.exec(this)) !== null) {
+    while ((matchesArr = dataBind.exec(result)) !== null) {
         if (matchesArr[1] === 'content') {
-            result = this.replace(/(<[^\n\r\/]+>)(<\/[^\n\r]+>)/g, function (match, p1, p2) {
+            result = result.replace(/(<[^\n\r\/]+>)(<\/[^\n\r]+>)/g, function (match, p1, p2) {
                 return p1 + dataObj[matchesArr[2]] + p2;
             });
         } else {
             result = result.replace(/(<[^\n\r]+")(>[^\n\r]+>)/g, function (match, p1, p2) {
+                if (dataObj[matchesArr[2]] === undefined) {
+                    return p1 + ' ' + matchesArr[1] + '="' + '"' + p2;
+                }
                 return p1 + ' ' + matchesArr[1] + '="' + dataObj[matchesArr[2]] + '"' + p2;
             });
         }
